65153490ec02e2eed712b147c11c7740d7473328
MariaPilarPerez <mdpperezgy@fpvirtualaragon.es>
2025-12-02 09:57:47 +0100

    He intentado Toast pero no funciona. La pantalla sale bien.

--- app/src/main/java/com/campusdigitalfp/filmoteca/MainActivity.kt
+++ app/src/main/java/com/campusdigitalfp/filmoteca/MainActivity.kt
@@ -1,17 +1,38 @@
 package com.campusdigitalfp.filmoteca
 
+import android.content.Context
 import android.os.Bundle
+import android.view.textclassifier.ConversationActions
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.horizontalScroll
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalContext
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.campusdigitalfp.filmoteca.ui.theme.FilmotecaTheme
+import androidx.compose.ui.platform.LocalContext
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,28 +41,61 @@ class MainActivity : ComponentActivity() {
         setContent {
             FilmotecaTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    AboutScreen()
                 }
             }
         }
     }
 }
-
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun AboutScreen(modifier: Modifier=Modifier){
+    //columna con elementos Creado por, seguido de imagen y seguido de row con dos botones
+// y otro elemento de la columna al final que es un boton
+    Column(modifier
+        .fillMaxWidth()
+        .fillMaxHeight(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+        )
+       {
+        Text(text="Creado por María Pilar Pérez Gómez",
+            fontSize = 16.sp
+            )
+        Spacer(modifier.height(16.dp))
+        Image( painter = painterResource(R.drawable.perfil), contentDescription = "Foto de perfil")
+        Spacer(modifier.height(8.dp))
+        Row(
+            modifier.padding(8.dp),
+            horizontalArrangement = Arrangement.Absolute.Center
+           )
+           {
+            Button(onClick = { //showToast()//
+                },
+                modifier.weight(1f))  {
+                Text(text= stringResource(R.string.ir_al_sitio_web))}
+               Spacer(modifier.width(8.dp))
+               Button(onClick = {}, modifier.weight(1f)) {
+                Text(text= stringResource(R.string.obtener_soporte))
+            }
+        }
+           Spacer(modifier.height(8.dp))
+        Button(onClick={}) {
+            Text(text= stringResource(R.string.volver))
+        }
+        }
+    }
+@Composable
+ fun showToast(context: Context, message: String){
+     val context = LocalContext.current
+     Toast.makeText(context, message, Toast.LENGTH_SHORT).show()
 }
+
+
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun AboutSreenPreview() {
     FilmotecaTheme {
-        Greeting("Android")
+       AboutScreen()
     }
 }
