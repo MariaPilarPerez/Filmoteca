05c7ac2413f6e4053d9356db24a45ad3920246ed
MariaPilarPerez <mdpperezgy@fpvirtualaragon.es>
2025-12-10 13:29:37 +0100

    Ejercicio 13: Interfaz de datos de una película

--- app/src/main/java/com/campusdigitalfp/filmoteca/screens/FilmDataScreen.kt
+++ app/src/main/java/com/campusdigitalfp/filmoteca/screens/FilmDataScreen.kt
@@ -1,110 +1,141 @@
 package com.campusdigitalfp.filmoteca.screens
 
+import android.content.Context
+import android.content.Intent
 import android.media.Image
+import android.net.Uri
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.modifier.modifierLocalConsumer
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
 import com.campusdigitalfp.filmoteca.R
 import com.campusdigitalfp.filmoteca.common.BarraSuperiorComun
 import com.campusdigitalfp.filmoteca.ui.theme.FilmotecaTheme
 
 //mostrar los datos de una pelicula
 // una columna con un texto y tres botones
-data class film (
-    val imagen: String = "interestelar",
-    val nombrefilm: String = "Interstellar",
-    val director: String ="Christopher Nolan",
-    val estreno: Int = 2014,
-    val genero: String ="Ciencia Ficción",
-    val formato: String ="DVD",
-    val enlaceIMDB: String ="https://m.imdb.com/es-es/title/tt0816692/?ref_=ext_shr_lnk"
-)
 
 
 @Composable
-fun FilmDataScreen(navController: NavHostController, titulo: String) {
-    val savedStateHandle = navController.currentBackStackEntry?.savedStateHandle
-    val context = LocalContext.current
-    val result: String? = savedStateHandle?.get("key_result")
+fun FilmDataScreen(navController: NavHostController, titulo: String)
 
-    Scaffold(topBar = { BarraSuperiorComun(navController, true)}, content = { padding ->
+{
+    val imagen: Painter = painterResource(R.drawable.interstellar)
+    val nombrefilm: String = "Interstellar"
+    val director: String ="Christopher Nolan"
+    val estreno: Int = 2014
+    val genero: String ="Ciencia Ficción"
+    val formato: String ="DVD"
+    val enlaceIMDB: String ="https://m.imdb.com/es-es/title/tt0816692/?ref_=ext_shr_lnk"
+    val context=LocalContext.current
 
-
+    Scaffold(topBar = {BarraSuperiorComun(navController,true)}, content = { padding ->
         Column(
             modifier = Modifier
+                .fillMaxWidth()
                 .fillMaxHeight()
-                .fillMaxWidth(),
-            verticalArrangement = Arrangement.Center,
+                .padding(top=100.dp),
+            verticalArrangement = Arrangement.Top,
             horizontalAlignment = Alignment.CenterHorizontally
         )
-
-
         {
-            Text(
-                text = titulo,
-                color = Color.Blue,
-                fontSize = 16.sp,
-                fontWeight = FontWeight.Bold
-
-            )
-            Text(
-                text = stringResource(R.string.datos_de_la_pelicula),
-                modifier = Modifier.padding(8.dp)
-            )
-            Spacer(modifier = Modifier.height(4.dp))
-            Button(onClick = { navController.navigate("datosfilm/$titulo") })
-            {
-                Text(text = stringResource(R.string.ver_pelicula_relacionada))
-            }
-            Button(onClick = { navController.navigate("editar") })
-            {
-                Text(text = stringResource(R.string.editar_pelicula))
-
-            }
-            Button(onClick = {
-                navController.navigate("lista")
-                {
-                    popUpTo("lista")
-                    { inclusive = true }
-                }
-            })
-            {
-                Text(text = stringResource(R.string.volver_a_la_principal))
-            }
-            if (!result.equals(null)) {
-                Text(modifier=Modifier.padding(top=16.dp),
-                    text = if (result.equals("RESULT_OK")) "Edición guardada"
-                    else "Edición Cancelada" //al haber solo dos resultados, me sirve la sentencia if
+            Row {
+                Spacer(modifier = Modifier.height(16.dp))
+                Image( painter = imagen
+                    , contentDescription = null,
+                    Modifier.size(200.dp)
                 )
+                Column() {
+                    Text(
+                        text = nombrefilm,
+                        fontWeight = FontWeight.Bold,
+                        fontSize = 24.sp
+                    )
+                    Text(
+                        text = "Director:",
+                        fontWeight = FontWeight.Bold,
+                        fontSize = 16.sp
+                    )
+                    Text(
+                        text = director,
+                        fontSize = 16.sp
+                    )
+                    Text(
+                        text = "Año de estreno:",
+                        fontWeight = FontWeight.Bold,
+                        fontSize = 16.sp
+                    )
+                    Text(
+                        text = estreno.toString(),
+                        fontSize = 16.sp
+                    )
+                    Text(
+                        text = genero + ", " + formato,
+                        fontSize = 16.sp
+                    )
+                }
+            }
+            Row {
+                Button(onClick = {VerEnIMDB(enlaceIMDB, context)},
+                    Modifier.weight(1f)
+                        .padding(8.dp))
+                {
+                    Text(
+                        text = "Ver en IMDB"
+                    )
+                }
+            }
+            Row {
+                Button(onClick = {navController.popBackStack()},
+                    Modifier.weight(1f)
+                    .padding(start= 8.dp)
+                    .padding(end=4.dp))
+                { Text(text = "Volver") }
+                Button(onClick = {}, Modifier.weight(1f)
+                    .padding(start=4.dp).padding(end=8.dp))
+                { Text(text = "Editar") }
             }
         }
     }
     )
+
+}
+
+fun VerEnIMDB(url: String, context: Context) {
+    val intent = Intent(Intent.ACTION_VIEW).apply {
+        data = Uri.parse(url)
+    }
+    context.startActivity(intent)
 }
 
 
-// en el enunciado del ejercicio 10: dice que deberemos navegar a otra pantalla pasando un argumento
-// con el nombre de la pelicula, pero creo que es lo que he puesto en la linea 51,
-//Button(onClick = {navController.navigate("datosfilm/$titulo")})
-//{
-//    Text(text = stringResource(R.string.ver_pelicula_relacionada))
-// que es volveer a mostrar la misma pantalla, ya que si no, termina la aplicación
